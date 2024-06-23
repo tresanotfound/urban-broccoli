5a96fb12e67ff46810634912a846bd83fcba135b
Treesa George <treesageorge76@gmail.com>
2024-06-23 20:46:53 +0530

    Update script.js

---
+++ web/script.js
@@ -0,0 +1,6 @@
+const btnElement = document.querySelector('button');
+const spanElement = document.getElementById('updateContent');
+btnElement.onclick = function(){
+    const yourName = prompt("Please Enter Your Name:");
+    spanElement.textContent = yourName;
+}
